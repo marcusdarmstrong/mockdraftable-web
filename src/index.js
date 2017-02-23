b4aba65d6406465a11f571cd3d5005d63c1b276c
Marcus Armstrong <marcusdarmstrong@Marcuss-MacBook-Pro.local>
2017-02-22 22:29:39 -0700

    Oh yeah

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import 'babel-polyfill';
 import express from 'express';
+import path from 'path';
 import type { $Request } from 'express';
 
 import compression from 'compression';
@@ -35,7 +36,7 @@ init().then((stores) => {
   app.set('x-powered-by', false);
   app.set('port', (process.env.PORT || 5000));
   app.use(compression());
-  app.use('/public', express.static('public', {
+  app.use('/public', express.static(path.join(__dirname, '..', 'public'), {
     maxAge: 1000 * 60 * 60 * 24 * 365, // one year
   }));
   app.use(favicon(`${__dirname}/../public/favicon.ico`));
