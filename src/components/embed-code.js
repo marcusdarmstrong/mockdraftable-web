02ab70efc816064b3f4d3470e2aea7b14b6b800b
Marcus Armstrong <marcusdarmstrong@Marcuss-MacBook-Pro.local>
2017-02-23 21:41:45 -0700

    Forgot a file

---
+++ src/components/embed-code.js
@@ -0,0 +1,26 @@
+// @flow
+
+import React from 'react';
+import { connect } from 'react-redux';
+
+import type { PlayerId, PositionId } from '../types/domain';
+
+type Props = {
+  selectedPlayerId: PlayerId,
+  selectedPositionId: PositionId,
+};
+
+const EmbedCode = ({ selectedPlayerId, selectedPositionId }: Props) => <div>
+  <p>To embed this player&apos;s information onto your site, just use one of these html snippets</p>
+  <h6>To highlight the player&apos;s spider graph, use:</h6>
+  <pre>{`<iframe src="http://www.mockdraftable.com/embed/${selectedPlayerId}?position=${selectedPositionId}&page=GRAPH" width="480" height="750" frameborder="0" scrolling="no"></iframe>`}</pre>
+  <h6>To highlight the player&apos;s measurements, use:</h6>
+  <pre>{`<iframe src="http://www.mockdraftable.com/embed/${selectedPlayerId}?position=${selectedPositionId}&page=MEASURABLES" width="480" height="750" frameborder="0" scrolling="no"></iframe>`}</pre>
+  <h6>To highlight the player&apos;s comparisons, use:</h6>
+  <pre>{`<iframe src="http://www.mockdraftable.com/embed/${selectedPlayerId}?position=${selectedPositionId}&page=COMPARISONS" width="480" height="750" frameborder="0" scrolling="no"></iframe>`}</pre>
+</div>;
+
+export default connect(state => ({
+  selectedPlayerId: state.selectedPlayerId,
+  selectedPositionId: state.selectedPositionId,
+}))(EmbedCode);
