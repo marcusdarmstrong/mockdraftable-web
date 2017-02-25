a99fb81eed842cd4731e46b7eacfaa7aa42d33dc
Marcus Armstrong <marcusdarmstrong@Marcuss-MacBook-Pro.local>
2017-02-24 20:50:11 -0700

    Copy fix

--- src/components/home-page.js
+++ src/components/home-page.js
@@ -9,8 +9,8 @@ export default () => <div className="window container-fluid col-12 col-lg-10 off
     <div className="col-12 col-lg-6 offset-lg-3">
       <p className="lead text-center">
         MockDraftable is the most comprehensive tool on the internet for NFL combine-based player
-        &nbsp;analysis and data visualization. Take a look around. You&apos;ll be surprised what
-        &nbsp;you discover.
+        analysis and data visualization. Take a look around. You&apos;ll be surprised what
+        you discover.
       </p>
       <p>
         <a href="/search" className="btn btn-lg btn-success btn-block">Search the Database &raquo;</a>
