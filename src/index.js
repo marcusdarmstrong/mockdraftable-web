22fb3419fc0328e53db7adb20f62b67530a4b27b
Marcus Armstrong <marcusdarmstrong@Marcuss-MacBook-Pro.local>
2017-02-27 16:44:10 -0700

    Remove ACME test code

--- src/index.js
+++ src/index.js
@@ -79,10 +79,6 @@ init().then((stores) => {
     res.send(JSON.stringify(await api.fetchTypeAheadResults(req.query.search)));
   });
 
-  app.get('/.well-known/acme-challenge/sJI2fruuWtN3goKIyUSp25S4F7O8dbs0d7baPjPkSEM', (req: $Request, res) => {
-    res.send('sJI2fruuWtN3goKIyUSp25S4F7O8dbs0d7baPjPkSEM.i8Z5Sr5IA3VD1phPjT0oWE2bVrnLwpaGPE9KjcK9M_I');
-  });
-
   const validPositions = positions.filter(pos => !!stores.positionEligibilityStore.get(pos.id));
   const posById = validPositions.reduce((a, pos) => Object.assign({}, a, { [pos.id]: pos }), {});
 
