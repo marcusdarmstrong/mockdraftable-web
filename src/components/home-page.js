b2ca1253f91e71d58695de862a10848a72a3d758
Marcus Armstrong <marcusdarmstrong@gmail.com>
2017-03-10 22:10:19 -0700

    Fixing my button colors

--- src/components/home-page.js
+++ src/components/home-page.js
@@ -13,7 +13,7 @@ export default () => <div className="window container-fluid col-12 col-lg-10 off
         you discover.
       </p>
       <p>
-        <a href="/search" className="btn btn-lg btn-success btn-block">Explore the Database &raquo;</a>
+        <a href="/search" className="btn btn-lg btn-primary btn-block">Explore the Database &raquo;</a>
       </p>
       <p className="text-center">
         Or take a look at <a href="/positions">Aggregate Data</a>.
