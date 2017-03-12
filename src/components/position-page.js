81d73ad924a35e18660d67a9f405feff450cf14f
Marcus Armstrong <marcusdarmstrong@gmail.com>
2017-03-12 08:58:57 -0600

    Adding the position selector to the position page

--- src/components/position-page.js
+++ src/components/position-page.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 
+import PositionSelector from './position-selector';
 import PositionStatsSection from './position-stats-section';
 
 type Props = {
@@ -12,6 +13,7 @@ export default ({ positionName }: Props) =>
   <div className="window container-fluid col-12 col-lg-10 offset-lg-1">
     <div className="row">
       <div className="col-12 col-lg-6 offset-lg-3">
+        <PositionSelector />
         <h3>Aggregate Statistics</h3>
         <h6 className="text-muted">{positionName}</h6>
         <PositionStatsSection />
